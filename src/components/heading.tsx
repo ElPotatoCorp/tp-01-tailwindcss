a20c0133163f08ef6741ce87bcbe3869e76ec5f1
Noah <noahgauche321@gmail.com>
2026-09-11 11:18:03 +0200

    Improved Heading modularity

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -2,26 +2,35 @@ import type { FC, ReactNode } from "react";
 import { tv, type VariantProps } from "tailwind-variants";
 
 type Props = VariantProps<typeof heading> & {
-  as: "h1" | "h2" | "h3";
+  as: "h1" | "h2" | "h3" | "span";
   children?: ReactNode;
 };
 
 const heading = tv({
-  base: 'font-bold',
   variants: {
     color: {
-      primary: 'text-secondary',
+      primary: 'text-primary',
+      secondary: 'text-secondary',
+      white: 'text-white',
+      black: 'text-black',
     },
     size: {
-      display: 'text-[96px]',
-      lg: 'text-[48px]',
-      md: 'text-[30px]',
-      sm: 'text-[20px] font-semibold',
+      display: 'text-[64px] md:text-[96px]',
+      lg: 'text-[32px] md:text-[48px]',
+      md: 'text-[16px] md:text-[30px]',
+      sm: 'text-[8px] md:text-[20px] font-semibold',
+    },
+    weight: {
+      bold: 'font-bold',
+      semi: 'font-semibold',
+      medium: 'font-medium',
+      normal: 'font-normal',
     },
   },
   defaultVariants: {
     color: 'primary',
     size: 'display',
+    weight: 'bold',
   },
 });
 

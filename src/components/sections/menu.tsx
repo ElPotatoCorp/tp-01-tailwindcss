6b348ee802ddd5d0606c7b6c669e8a15cc11c9ee
Noah <noahgauche321@gmail.com>
2026-09-13 20:05:52 +0200

    Responsive Menu

--- src/components/sections/menu.tsx
+++ src/components/sections/menu.tsx
@@ -2,12 +2,12 @@ import type { FC } from "react";
 
 export const Menu: FC = function () {
   return (
-    <nav className="grid grid-cols-5 w-1/2 mx-auto my-10 text-2xl font-semibold text-center">
-      <a href="#activites">Activités</a>
-      <a href="#experiences">Expériences</a>
-      <a href="#" className="font-black">Açores</a>
-      <a href="#hotels">Hôtels</a>
-      <a href="#spa">Spa</a>
+    <nav className="sm:grid sm:grid-cols-5 flex justify-center w-1/2 mx-auto my-10 text-2xl font-semibold">
+      <a href="#activites" className="hidden sm:block">Activités</a>
+      <a href="#experiences" className="hidden sm:block">Expériences</a>
+      <a href="#" className="font-black self-center">Açores</a>
+      <a href="#hotels" className="hidden sm:block">Hôtels</a>
+      <a href="#spa" className="hidden sm:block">Spa</a>
     </nav>
   );
 };

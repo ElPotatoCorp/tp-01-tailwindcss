9cbbedfb6574927910abd807b7e765ded13fec36
Noah <noahgauche321@gmail.com>
2026-09-13 20:15:15 +0200

    Responsive Hero

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,8 +3,8 @@ import { Heading } from "../heading";
 
 export const Hero: FC = function () {
   return (
-    <div className="# mx-8 my-4 overflow-hidden rounded-2xl bg-white">
-      <div className="relative aspect-video">
+    <div className="mx-4 my-2 sm:mx-8 sm:my-4 overflow-hidden rounded-2xl bg-white">
+      <div className="relative aspect-card sm:aspect-video">
         <img
           src="img/cover.jpeg"
           alt="Hero"
@@ -12,12 +12,12 @@ export const Hero: FC = function () {
         />
 
         <div className="absolute inset-10 flex flex-col justify-between">
-          <div className="flex flex-col text-primary leading-tight">
+          <div className="flex flex-col text-center sm:text-start text-primary leading-tight">
             <Heading as="span" size="sm" weight="normal" uppercase>Bienvenue aux</Heading>
             <Heading as="h1" uppercase>Açores</Heading>
             <Heading as="span" size="sm" weight="medium">Joyau de verdure perdu dans l'Atlantique</Heading>
           </div>
-          <button className="self-start text-emerald-700 bg-white rounded-xl px-8 py-3 text-center font-semibold">
+          <button className="self-center sm:self-start text-emerald-700 bg-white rounded-xl px-8 py-3 text-center font-semibold">
             Réserver
           </button>
         </div>
